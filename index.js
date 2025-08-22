363e18fc0a88dbdcef281218ccdc61feb262de85
Divyanshu-176 <divyanshujain176@gmail.com>
2025-08-22 23:17:01 +0530

    Adding skeleton routes

--- index.js
+++ index.js
@@ -6,12 +6,52 @@ const mongoose = require("mongoose")
 const app = express()
 
 
+app.post("/user/signup", (req,res)=>{
+    res.json({
+        msg:"user signup"
+    })
+
+})
 
 
 
+
+app.post("/user/signin", (req,res)=>{
+    res.json({
+        msg:"user signin"
+    })
+})
+
+
+
+
+app.get("/user/purchases", (req,res)=>{
+    res.json({
+        msg:"user purchased courses"
+    })
+})
+
+
+
+
+app.get("/course/purchase", (req,res)=>{
+    res.json({
+        msg:"user purchasing course"
+    })
+})
+ 
+
+
+
+app.get("/courses", (req,res)=>{
+    res.json({
+        msg:"All the courses"
+    })
+})
 
 
 async function server(){
     await app.listen(3000)
     await console.log("Server started at port: 3000")
 }
+server()
