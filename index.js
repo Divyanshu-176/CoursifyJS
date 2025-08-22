ee40ab18e930debd8b8da11b39818a54a83ff9fe
Divyanshu-176 <divyanshujain176@gmail.com>
2025-08-22 19:08:14 +0530

    adding dependencies and creating project skeleton

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-fiuhfiuh
+const express = require("express")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
+
+const app = express()
+
+
+
+
+
+
+
+async function server(){
+    await app.listen(3000)
+    await console.log("Server started at port: 3000")
+}
