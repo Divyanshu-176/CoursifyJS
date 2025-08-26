547a11b6e394689d4da0ce1575c638d7c4f886ce
Divyanshu-176 <divyanshujain176@gmail.com>
2025-08-26 22:31:06 +0530

    Added signup & signin EP in Admin and User files

--- routes/admin.js
+++ routes/admin.js
@@ -4,20 +4,67 @@ const {adminModel} = require("../db")
 const {adminAuth} = require("../auth-middlewares/adminMiddleware")
 const bcrypt = require("bcrypt")
 
+const jwt = require("jsonwebtoken")
+const JWT_ADMIN_SECRET ="ADMIN_SECRET"
+
+
+
+
+
+
 
 adminRouter.post("/signup", async (req,res)=>{
-    res.json({
-        msg:"Admin signup"
+    const {email,password,name} = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    try {
+        await adminModel.create({
+            email,
+            password: hashedPassword,
+            name
+        }) 
+
+         res.json({
+        msg:"Admin signup succeeded"
     })
+    } catch (error) {
+        res.status(400).json({
+            msg:`Cant Signup, error: ${error}`
+        })
+    }   
 })
 
 
 
 
-adminRouter.post("/signin",(req,res)=>{
-    res.json({
-        msg:"Admin signin"
+adminRouter.post("/signin", async (req,res)=>{
+    const {email, password} = req.body
+    try {
+        const admin = await adminModel.findOne({
+        email
     })
+    const hashCompare = await bcrypt.compare(password, admin.password)
+
+    if(admin && hashCompare){
+        const token = jwt.sign({
+            id: admin._id
+        }, JWT_ADMIN_SECRET)
+
+
+        res.json({
+            token:token,
+            msg:"Admin Signin Succeeded"
+        })
+    }else{
+        res.status(400).json({
+            msg:"Invalid Credentials"
+        })
+    }
+    } catch (error) {
+        res.json({
+            msg:`Something went wrong: ${error}`
+        })
+    }  
 })
 
 
--- routes/user.js
+++ routes/user.js
@@ -2,23 +2,69 @@ const {Router} = require("express");
 const userRouter = Router()
 const {userModel} = require("../db")
 const {userAuth} = require("../auth-middlewares/userMiddleware")
+const bcrypt = require("bcrypt")
+
+const jwt = require("jsonwebtoken")
+const JWT_USER_SECRET ="USER_SECRET"
+
+
+
+
+
 
 
 userRouter.post("/signup", async (req,res)=>{
-   
-    res.json({
-        msg:"user signup"
-    })
+    const {email,password,name} = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
 
+    try {
+        await userModel.create({
+            email,
+            password: hashedPassword,
+            name
+        }) 
+
+         res.json({
+        msg:"User signup succeeded"
+    })
+    } catch (error) {
+        res.status(400).json({
+            msg:`Cant Signup, error: ${error}`
+        })
+    } 
 })
 
 
 
 
-userRouter.post("/signin", (req,res)=>{
-    res.json({
-        msg:"user signin"
-    })
+userRouter.post("/signin", async (req,res)=>{
+    const {email, password} = req.body
+        try {
+            const user = await userModel.findOne({
+            email
+        })
+        const hashCompare = await bcrypt.compare(password, user.password)
+    
+        if(user && hashCompare){
+            const token = jwt.sign({
+                id: user._id
+            }, JWT_USER_SECRET)
+    
+    
+            res.json({
+                token:token,
+                msg:"User Signin Succeeded"
+            })
+        }else{
+            res.status(400).json({
+                msg:"Invalid Credentials"
+            })
+        }
+        } catch (error) {
+            res.json({
+                msg:`Something went wrong: ${error}`
+            })
+        }
 })
 
 
